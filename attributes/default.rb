5d88fbe0e5f80aa8026c793540be05e1eb8b35a5
Dan Jakob Ofer <dan@ofer.to>
2018-05-02 20:59:03 -0400

    Explicitly target the url load.transit.tips

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,6 +1,6 @@
 default['nginx']['server_names_hash_bucket_size'] = '128'
 
-default['reverse_proxy']['regex_url'] = '~^(www\.)?transit\.tips$'
+default['reverse_proxy']['regex_url'] = '~^load\.transit\.tips$'
 default['reverse_proxy']['url'] = 'https://www.transit.tips'
 
 default['reverse_proxy']['client']['url'] = 'https://client.transit.tips'
